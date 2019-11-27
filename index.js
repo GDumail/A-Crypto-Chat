7019e5c61c4bd31e35c96417c21bbdef5a55d220
Gwendal Dumail <gwendal.dumail@epitech.eu>
2019-11-27 11:52:42 +0100

    feat: simple socket.io chat

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const port = process.env.A_CRYPTO_CHAT_PORT | 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+http.listen(port, () => {
+    console.log("listening on *:" + port);
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+    socket.on("chat message", (msg) => {
+        console.log("message: " + msg);
+        io.emit('chat message', msg);
+    });
+});
